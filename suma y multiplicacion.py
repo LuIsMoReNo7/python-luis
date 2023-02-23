43bb5307996a35a3db39e40d9807514f3dc6191f
LuIsMoReNo7 <1483853@senati.pe>
2023-02-23 11:00:22 -0500

    trabajo 2 de python

---
+++ suma y multiplicacion.py
@@ -0,0 +1,11 @@
+#Suma y multiplicacion
+
+#ingrese los cuatro valores
+v1=int(input("ingrese el valor 1 : "))
+v2=int(input("ingrese el valor 2 : "))
+v3=int(input("ingrese el valor 3 : "))
+v4=int(input("ingrese el valor 4 : "))
+
+#obtenemos la sumatoria
+suma = v1 + v2 + v3 + v4
+
