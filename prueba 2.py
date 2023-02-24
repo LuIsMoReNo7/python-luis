e8aef4e93359d7d7aac1af471a5b6c83c9b5e62c
LuIsMoReNo7 <1483853@senati.pe>
2023-02-24 13:15:26 -0500

    trabajo de prueba para corregir

---
+++ prueba 2.py
@@ -0,0 +1,17 @@
+#crear un programa que genere la suma el numero actual y el anterior
+
+#solicitamos que ingrese la cantidad de series
+n=int(input("ingrese la cantidad de series : "))
+#ponemos los primeros valores
+actual = int(input("ingrese el valor actual : "))
+anterior = int(input("ingrese el valor anterior : "))
+#mostramos el primer valor
+print("la suma =  ",end="")
+#asignamos una variable de control
+s = 0
+#generamos la serie
+for s in range(1,n):
+      print(n,"+",n-1, "0",n+(n-1))
+      
+     
+     
