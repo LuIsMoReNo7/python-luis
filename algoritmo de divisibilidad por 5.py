78c6792efa184b49d59d04fb81df6baf6aebd0e3
LuIsMoReNo7 <1483853@senati.pe>
2023-02-24 14:25:55 -0500

    obtenemos los numeros divisibles por 5

--- algoritmo de divisibilidad por 5.py
+++ algoritmo de divisibilidad por 5.py
@@ -2,7 +2,14 @@
 
 números = [10, 14, 20, 31, 35, 40, 45, 52, 57]
 
+#hacemos uso de lambda y filter
+
 divisible_por_5 = lambda x: x % 5 == 0
 
 numeros_div_5 = filter(divisible_por_5, números)
+
+#obtenemos la lista de los numeros divisibles por 5
+
+print(list(numeros_div_5))
+
 
