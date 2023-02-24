80d369e06a562a9466d50779bd32570e7fb1383a
LuIsMoReNo7 <1483853@senati.pe>
2023-02-24 14:33:13 -0500

    explicacion de lambda y filter

--- algoritmo de divisibilidad por 5.py
+++ algoritmo de divisibilidad por 5.py
@@ -3,7 +3,10 @@
 números = [10, 14, 20, 31, 35, 40, 45, 52, 57]
 
 #hacemos uso de lambda y filter
-
+#lambda: sirve por si una función es utilizada una sola vez,
+#lo mejor es usar una función lambda para evitar código innecesario y desorganizado.
+#filter: sirve para filtrar de forma eficiente los elementos usando una 
+#función que proporcionamos
 divisible_por_5 = lambda x: x % 5 == 0
 
 numeros_div_5 = filter(divisible_por_5, números)
