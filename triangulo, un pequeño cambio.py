20bff5114b890aea4b17c40d5f26fb3cab00496f
LuIsMoReNo7 <1483853@senati.pe>
2023-02-25 10:23:53 -0500

    un pequeño cambio

---
+++ triangulo, un pequeño cambio.py
@@ -0,0 +1,8 @@
+#usamo for y range para darle un rango especifico 
+for a in range(6):
+      for b in range(a):
+            #ya obteniendo la base, el programa estaia liso para ejecutarse..
+            #vamos a imprimir la variable "a"
+             #y usaremos la funcion "end=' ' " que sirve para dejar un espacio n vez de un salto de linea
+           print(a, end=' ') 
+      print()
